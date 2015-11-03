deaba1bbeaecae7e2f4a94c4196dd79efe4d220c
jlyons49 <josh@adeptint.com>
2015-11-03 18:32:45 -0500

    Client file open

--- gen-cpp/Something_client.cpp
+++ gen-cpp/Something_client.cpp
@@ -5,6 +5,9 @@
 #include <transport/TBufferTransports.h>
 #include <protocol/TBinaryProtocol.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
@@ -15,15 +18,31 @@ using namespace std;
 int main(int argc, char **argv) {
 
 	Url_req   request;
+	FILE      *fp;
 	string    _return;
 	int       mode = 0;
 	int       ret = 0;
 
-	if( argc == 2 ) {
+	else if( argc == 2 ) {
 		request.url = argv[1];
 		mode = 1;
 	}
+	else if( argc == 3 ) {
+		// Open file and pick load mode
+		fp = fopen (argv[2],"r");
+		if ( fp == NULL ) {
+			printf("File failed to open!\n");
+			return 0;
+		}
+		if(atoi(argv[3]) == 0) {
+			printf("Invalid mode given!\n");
+			return 0;
+		}
+		mode = 2 + atoi(argv[3]);
+		
+	}
 	else {
+		printf("Usage: something_client [web_address] | [address_file load_mode]\n Pinging\n");
 		mode = 0;
 	}
 
