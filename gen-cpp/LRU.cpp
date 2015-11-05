d1615db5e80c1f9e761e85340490f3bc07d9854d
jlyons49 <josh@adeptint.com>
2015-11-04 22:19:28 -0500

    Revert "Revert "LRU files""
    This reverts commit 9d6d77803ecdefd38929314fc025b7ec2275e02e.

---
+++ gen-cpp/LRU.cpp
@@ -0,0 +1,16 @@
+#include "LRU.h"
+
+using namespace std;
+
+Lru::Lru(int s): Cache(s) {
+	currSize = 0;
+}
+		
+string Lru::get(string key){
+	return "";
+}
+		
+void Lru::set(string key, string value){
+}
+	
+
